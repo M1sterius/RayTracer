ec610a2aab96777accd3f612f1ae25f7b41f6771
Misterius <kiger3@yandex.ru>
2024-09-25 16:16:35 +0300

    Implemented uniforms finding and setting support for compute shaders

--- Source/GL/ComputeShader.cpp
+++ Source/GL/ComputeShader.cpp
@@ -1,6 +1,7 @@
 #include "ComputeShader.hpp"
 #include "ShaderSourceProcessing.h"
 #include "glad.h"
+#include "gtc/type_ptr.hpp"
 
 ComputeShader::ComputeShader(const std::string& source)
     : m_Handle(0)
@@ -50,4 +51,89 @@ void ComputeShader::Unbind() const
 void ComputeShader::Dispatch(const size_t x, const size_t y, const size_t z)
 {
     glDispatchCompute(x, y, z);
+}
+
+int ComputeShader::FindUniform(const std::string &name) const
+{
+    if (m_UniformsLocationCache.find(name) != m_UniformsLocationCache.end())
+        return m_UniformsLocationCache[name];
+
+    const int location = glGetUniformLocation(m_Handle, name.c_str());
+    if (location != -1)
+    {
+        m_UniformsLocationCache[name] = location;
+        return location;
+    }
+    std::cout << "Unable to find uniform named: " << name << '\n';
+    return -1;
+}
+
+void ComputeShader::SetUniform1is(const std::string& name, const int value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform1i(location, value);
+}
+
+void ComputeShader::SetUniform1i(const std::string& name, const unsigned int value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform1ui(location, value);
+}
+
+void ComputeShader::SetUniform1i(const std::string& name, const size_t value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform1ui(location, value);
+}
+
+void ComputeShader::SetUniform1f(const std::string& name, const float value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform1f(location, value);
+}
+
+void ComputeShader::SetUniformBool(const std::string &name, const bool value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform1i(location, value);
+}
+
+void ComputeShader::SetUniformVec2(const std::string& name, const glm::vec2& value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform2f(location, value.x, value.y);
+}
+
+void ComputeShader::SetUniformVec3(const std::string& name, const glm::vec3& value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform3f(location, value.x, value.y, value.z);
+}
+
+void ComputeShader::SetUniformVec4(const std::string& name, const glm::vec4& value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniform4f(location, value.x, value.y, value.z, value.w);
+}
+
+void ComputeShader::SetUniformMat3(const std::string& name, const bool transpose, const glm::mat3& value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniformMatrix3fv(location, 1, transpose, glm::value_ptr(value));
+}
+
+void ComputeShader::SetUniformMat4(const std::string& name, const bool transpose, const glm::mat4& value) const
+{
+    const int location = FindUniform(name);
+    if (location == -1) return;
+    glUniformMatrix4fv(location, 1, transpose, glm::value_ptr(value));
 }
--- Source/GL/ComputeShader.hpp
+++ Source/GL/ComputeShader.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "glm.hpp"
+
 #include <string>
 #include <cstdint>
+#include <unordered_map>
 
 class ComputeShader
 {
@@ -17,9 +20,23 @@ public:
     void Unbind() const;
 
     void Dispatch(const size_t x, const size_t y, const size_t z);
+
+    int FindUniform(const std::string& name) const;
+
+    void SetUniform1is(const std::string& name, const int value) const;
+    void SetUniform1i(const std::string& name, const unsigned int value) const;
+    void SetUniform1i(const std::string& name, const size_t value) const;
+    void SetUniform1f(const std::string& name, const float value) const;
+    void SetUniformBool(const std::string& name, const bool value) const;
+    void SetUniformVec2(const std::string& name, const glm::vec2& value) const;
+    void SetUniformVec3(const std::string& name, const glm::vec3& value) const;
+    void SetUniformVec4(const std::string& name, const glm::vec4& value) const;
+    void SetUniformMat3(const std::string& name, const bool transpose, const glm::mat3& value) const;
+    void SetUniformMat4(const std::string& name, const bool transpose, const glm::mat4& value) const;
 private:
     explicit ComputeShader(const std::string& source);
 
     unsigned int m_Handle;
+    mutable std::unordered_map<std::string, int> m_UniformsLocationCache;
 };
 
